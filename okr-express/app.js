239e88f4c866dd2c279b2d06c2d4e17120457941
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-01-27 12:26:35 +0530

    Add: objective type single dummy data object

--- okr-express/app.js
+++ okr-express/app.js
@@ -1,10 +1,39 @@
-const express = require('express');
+import express from 'express';
+import {faker} from '@faker-js/faker';
+
 const app = express();
 
+let mockObjective = {
+    id: faker.string.uuid(),
+    objective: faker.company.buzzNoun(),
+    keyResults: [
+        {
+            title: faker.company.buzzNoun(),
+            initialValue: faker.number.int({min: 0, max: 0}),
+            currentValue: faker.number.int({min: 0, max: 10}),
+            targetValue: faker.number.int({min: 10, max: 10}),
+            metric: faker.company.buzzNoun(),
+        }
+    ]
+}
+
+let dummyData = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    bio: faker.person.bio(),
+    age: faker.number.int({
+        min: 1,
+        max: 100,
+    }),
+}
+
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World!");
+    console.log(dummyData);
+    res.json(mockObjective);
 })
 
-app.listen(3000, ()=>{
+
+const PORT = 3000;
+app.listen(PORT, () => {
     console.log("Server running on port 3000");
 });
