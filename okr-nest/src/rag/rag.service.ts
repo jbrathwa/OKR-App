e0a61204234f44bf834c8c780a3d4f6d723b37fd
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-02-09 17:17:49 +0530

    Fix: System and User Prompt to get accurate expected result.

--- okr-nest/src/rag/rag.service.ts
+++ okr-nest/src/rag/rag.service.ts
@@ -1,33 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import {AzureChatOpenAI} from "@langchain/openai";
-import {ChatPromptTemplate} from "@langchain/core/prompts";
+import { AzureChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
 
 @Injectable()
 export class RagService {
+  model: AzureChatOpenAI;
 
-    model: AzureChatOpenAI
-    constructor() {
-        this.model = new AzureChatOpenAI({
-            azureOpenAIApiKey: process.env.API_KEY,
-            model: process.env.MODEL,
-            azureOpenAIApiInstanceName: process.env.INSTANCE,
-            azureOpenAIApiDeploymentName:
-                process.env.DEPLOYMENT,
-            azureOpenAIApiVersion: process.env.API_VERSION,
-        });
-    }
+  constructor() {
+    this.model = new AzureChatOpenAI({
+      azureOpenAIApiKey: process.env.API_KEY,
+      model: process.env.MODEL,
+      azureOpenAIApiInstanceName: process.env.INSTANCE,
+      azureOpenAIApiDeploymentName: process.env.DEPLOYMENT,
+      azureOpenAIApiVersion: process.env.API_VERSION,
+    });
+  }
 
-    async get(objective: string){
-        const promptTemplate = ChatPromptTemplate.fromMessages([
-            ['system', "Generate single keyResults object for user given objective. key result must contain only following properties: title (must be short and just only of max 2-3 word(s)), metrics (what metrics used in keyresult), and initial, completed and target must be number."],
-            ['user', '{text} Give keyResults of give objective in the form of only json string. Important Note: Dont give me other explanation or anything, i just want json string only not in md format not in any other format only in json string text'],
-        ]);
+  async get(objective: string) {
+    const promptTemplate = ChatPromptTemplate.fromMessages([
+      [
+        'system',
+        'Generate single keyResults object for user given objective. key result must contain only following properties: title (must be short and just only of max 3-5 word(s)), metric (what metrics used in keyresult), and initial, completed and target must be number. ',
+      ],
+      [
+        'user',
+        '{text} Give me keyResults of give objective in the form of only json string. Important Note: Dont give me other explanation or anything, Make sure that you generate output must contain these keys name only (title, initialValue, targetValue, currentValue, metric) and i just want json string only not in md format not in any other format only in json string text. Must follow this: Dont give me ```json``` i only want json string of data and only give just one object not multiple in array form.',
+      ],
+    ]);
 
-        const promptValue = await promptTemplate.invoke({
-            text: objective,
-        });
+    const promptValue = await promptTemplate.invoke({
+      text: objective,
+    });
 
-        const response = await this.model.invoke(promptValue);
-        return JSON.parse(response.content as string);
-    }
+    const response = await this.model.invoke(promptValue);
+    console.log(response.content);
+    return JSON.parse(response.content as string);
+  }
 }
